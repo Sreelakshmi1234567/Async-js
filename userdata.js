eec736a96164578984c84da3bd46b83f8df907ed
Sreelakshmi1234567 <sreelakshmi22254@gmail.com>
2024-09-05 21:20:08 +0530

    first commit

---
+++ userdata.js
@@ -0,0 +1,21 @@
+
+
+
+function fetchUserData() {
+    return new Promise((resolve) => {setTimeout(() => {resolve({ name: "sree", age: 20, place: "pkd" })}, 2000); 
+    });
+  }
+  
+  async function getUserData() {
+    const user = await fetchUserData(); 
+    return user; 
+  }
+  
+ 
+  async function UserData() {
+    const user = await getUserData(); 
+    console.log(user); 
+  }
+  
+  UserData();
+  
